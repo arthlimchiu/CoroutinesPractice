583221ffe8856f9fc8b4bb6a01f9ac5c567bcdb2
Arth Limchiu <limchiuarth@gmail.com>
2019-06-19 01:08:12 +0800

    basic runBlocking

--- app/src/main/java/com/arthlimchiu/coroutinespractice/MainActivity.kt
+++ app/src/main/java/com/arthlimchiu/coroutinespractice/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.arthlimchiu.coroutinespractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        runBlocking {
+            val job = launch {
+                doWorld()
+            }
+
+//            job.join()
+            Log.d("MainActivity", "Hello,")
+        }
+    }
+
+    private suspend fun doWorld() {
+        delay(1000L)
+        Log.d("MainActivity", "World!")
     }
 }
